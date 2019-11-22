6a5537007ca7cc5a7529f1675f244f8945af5d28
chinmaybhale <chinmay.bhale@live.com>
2019-11-21 23:29:53 -0700

    Connected buttons to controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    public Button save = new Button();
+    public Button open = new Button();
+
+    @FXML
+    public void saveButtonClicked(Event e) {
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Control;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        Controller controller = new Controller();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setController(controller);
+
+        Parent root = FXMLLoader.load(getClass().getResource("index.fxml"));
+        primaryStage.setTitle("Text Formatter");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
