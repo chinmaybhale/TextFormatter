95c9ab2e03c236d7d43d153621bfe4987384b3e6
anngo-asu <ango5@asu.edu>
2019-11-29 16:45:05 -0700

    added framework for sanitize method and added Formatter class variable

--- src/sample/Formatter.java
+++ src/sample/Formatter.java
@@ -1,10 +1,102 @@
 
-package sample;
-
+package TextFormatter;
 import java.util.ArrayList;
 
 class Formatter{
+    private int line_length;
+    private int align;  // left = 0, right, centered, equal
+    private boolean wrap;
+    private boolean double_spaced;
+    private boolean title;  // switched off after first text line
+    private int p; // = 0 after insert
+    private int b; // = 0 after insert
+    private boolean two_column;
 
+    // sanitize: mark all commands causing errors and throws errors
+    //           then delete error commands and return cleaned input
+    public String sanitize(String inputStrings){
+        String[] lines = inputStrings.split("\\r?\\n");
+        for(int i = 0; i < lines.length; i++) {
+            if(lines[i].startsWith("-")){
+                switch(lines[i].trim()){
+                    case "-l":  // left-justified
+                        align = 0;
+                        break;
+                    case "-r":  // right-justified
+                        align = 1;
+                        break;
+                    case "-c":  // centered
+                        align = 2;
+                        break;
+                    case "-e":  // equally spaced
+                        align = 3;
+                        break;
+                    case "-w+": // wrap on
+                        wrap = 1;
+                        break;
+                    case "-w-": // wrap off
+                        wrap = 0;
+                        break;
+                    case "-s":  // single spaced
+                        double_spaced = false;
+                        break;
+                    case "-d":  // double spaced
+                        double_spaced = true;
+                        break;
+                    case "-t":  // title
+                        //if title fits with line length
+                        // HOW DO I CHECK THIS BC I HAVE TO
+                        // LOOK AHEAD?? :((
+                        break;
+                    case "-a1":  // 1 column
+                        two_column = false;
+                        break;
+                    case "-a2": // 2 columns
+                        two_column = true;
+                        break;
+                    default:
+                        //check if -n, -p, or -b w/ valid command
+                        switch(lines[i].substring(0,2)){
+                            case "-n":
+                                if(two_column){
+                                    try{
+                                        line_length = Integer.parseInt(lines[i].substring(2));
+                                    }
+                                    catch (NumberFormatException e){
+                                        //throw invalid line length error
+                                    }
+                                }
+                                catch (Error e){
+                                    //throw cannot change line length bc of column error
+                                }
+                                break;
+                            case "-p": // insert spaces
+                                if(align != 0) {
+                                    try {
+                                        Integer.parseInt(lines[i].substring(2))
+                                    } catch (NumberFormatException e) {
+                                        //throw invalid num option
+                                    }
+                                }
+                                else{
+                                    //throw can't insert space bc of alignment error
+                                }
+                                break;
+                            case "-b": //insert blank lines
+                                try {
+                                    Integer.parseInt(lines[i].substring(2))
+                                }
+                                catch (NumerFormatException e){
+                                    //throw invalid num option
+                                }
+                                break;
+                            default:
+                        }
+
+                }
+            }
+        }
+    }
     public static String formatColumns(String inputString){
         ArrayList<ColumnBlock> blocks = new ArrayList<ColumnBlock>();
         ColumnBlock current = new ColumnBlock(1);
